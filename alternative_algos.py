18b534bd8b4ce102eb424a85df7200e475b86185
BalzNash <73427876+BalzNash@users.noreply.github.com>
2021-01-01 17:12:22 +0100

    added rq5

--- alternative_algos.py
+++ alternative_algos.py
@@ -2,6 +2,11 @@ import networkx as nx
 from collections import defaultdict
 import numpy as np
 import random
+from tqdm import tqdm
+import cProfile, pstats, io, random
+
+
+#---------------PREPROCESSING------------------
 
 graph = nx.DiGraph()
 # for each link in links add edge
@@ -43,6 +48,7 @@ for line in open('wiki-topcats-page-names.txt', 'r').readlines():
     page_names.append(line[i+1:].replace('\n', ''))
 
 
+# enumerate categories
 inverted_page_names = dict()
 for index, name in enumerate(page_names):
     inverted_page_names[name] = index
@@ -150,18 +156,130 @@ def max_flow(source, sink, g):
     
     return max_flow
 
-# u = inverted_page_names['Zoe Saldana']
-# v = inverted_page_names['The Matrix']
-# print(max_flow(u,v,graph))
+# cat_1 = 'Southampton F.C. players'
+# cat_2 = 'English footballers'
+# # sg = my_double_cat_subg('Southampton F.C. players','English footballers', graph)
+# u = random.choice(categories[cat_1])
+# v = random.choice(categories[cat_2])
 
-cat_1 = 'Southampton F.C. players'
-cat_2 = 'English footballers'
-sg = my_double_cat_subg('Southampton F.C. players','English footballers', graph)
-u = random.choice(categories[cat_1])
-v = random.choice(categories[cat_2])
-
-print(max_flow(u,v,sg))
+#print(max_flow(u,v,graph))
 
 
+#------------------------RQ5-----------------------------
+
+def profile(fnc):
+
+    """a decorator that uses Cprofile to profile a function"""
+
+    def inner(*args, **kwargs):
+        
+        pr = cProfile.Profile()
+        pr.enable()
+        retval = fnc (*args, **kwargs)
+        pr.disable()
+        s = io.StringIO()
+        sortby = 'cumulative'
+        ps = pstats.Stats(pr, stream=s).sort_stats(sortby)
+        ps.print_stats()
+        print(s.getvalue())
+        return retval
+
+    return inner
 
 
+def compute_node_distances(u, g):
+    """Uses BFS to compute all the distances from a node
+    Args:
+        u (int): starting node number
+        g (Networkx graph): graph
+
+    Returns:
+        [dict]: mapping from node number to distance from u
+    """
+    # keep track of explored nodes and their distances
+    explored = {}    
+    explored[u] = 0
+    # keep track of all the paths to be checked
+    queue = [u]
+    # keeps looping until all possible paths have been checked
+    while queue:
+        # pop the first path from the queue
+        node = queue.pop(0)
+            # go through all neighbour nodes, store their distances
+            # push them into the queue
+        for neighbour in g.neighbors(node):
+            if neighbour not in explored:
+                explored[neighbour] = explored[node] + 1
+                queue.append(neighbour)
+    return explored
+
+
+def compute_all_nodes_distances(category, graph):
+    """Computes all the distances from each node in a category
+
+    Args:
+        category (list): list of node numbers in a category
+        graph (Networkx graph): graph
+
+    Returns:
+        [dict of dicts]: mapping from all nodes in a category to all the distances from that node
+                         e.g. dist[n1][n2] is the distance from node n1 to node n2
+
+    TODO: convert dict of dicts to single dict with tuple (n1, n2) as a key, is it worth it? 
+    """
+    dist = {}
+    for n1 in category:
+        dist[n1] = compute_node_distances(n1, graph)
+    return dist
+
+#@profile
+def sort_categories_by_distance(c, categories, graph):
+    """Takes a category, returns the other categories sorted by their distance from the first one (closest first).
+       The distance is defined as the median of the distances between each node of the first category and the others
+
+    Args:
+        c (str): name of the starting category
+        categories (dict): mapping from all categories names to their nodes
+        graph (Networkx graph): graph
+
+    Returns:
+        [list of tuple]: sorted categories and their distance
+                         e.g. [(cat2, 3), (cat3, 5)]
+    """
+    other_cat = categories.copy()
+    del other_cat[c]
+    distances = compute_all_nodes_distances(categories[c], graph)
+    medians = []
+
+    for cat in tqdm(other_cat):
+        nodes = categories[cat]
+        my_list = []
+        for node in nodes:
+            for i in categories[c]:
+                if node in distances[i]:
+                    my_list.append(distances[i][node])
+                else:
+                    my_list.append(float("inf"))
+        medians.append((cat,np.median(np.array(my_list))))
+
+    return sorted(medians, key = lambda x: x[1])
+
+
+if __name__ == "__main__":
+
+    # RQ2
+
+    #print(exploring("Marty O'Brien", 2))
+
+    # RQ4
+
+    # cat_1 = 'Southampton F.C. players'
+    # cat_2 = 'English footballers'
+    # sg = my_double_cat_subg(cat_1, cat_2, graph)
+    # u = random.choice(categories[cat_1])
+    # v = random.choice(categories[cat_2])
+    # print(max_flow(u,v,graph))
+
+    # RQ5
+
+    print(sort_categories_by_distance('English footballers', categories, graph))
